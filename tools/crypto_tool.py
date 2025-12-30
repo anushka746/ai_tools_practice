0a6b8b65669f23a58c972ae101233eda3fb71f48
anushka746 <anushkakhare14@gmail.com>
2025-12-30 15:22:17 +0530

    Fixed bug in crypto price fetch and updated README

--- tools/crypto_tool.py
+++ tools/crypto_tool.py
@@ -2,6 +2,21 @@ import requests
 import os
 import json
 
+
+trusted_symbols = {
+    "btc": "bitcoin",
+    "eth": "ethereum",
+    "doge": "dogecoin",
+    "sol": "solana",
+    "xrp": "ripple",
+    "ada": "cardano",
+    "bnb": "binancecoin",
+    "matic": "polygon",
+    "ltc": "litecoin",
+    "dot": "polkadot"
+}
+
+
 CACHE_FILE = "coin_list_cache.json"
 
 def fetch_coin_list():
@@ -21,18 +36,30 @@ def build_symbol_map():
         with open(CACHE_FILE, "w", encoding="utf-8") as f:
             json.dump(coin_list, f)
 
-    symbol_map = {}
+    
+    name_map = {}
+
+
     for coin in coin_list:
-        symbol = coin.get("symbol", "").lower()
+        name = coin.get("name", "").lower()
         coin_id = coin.get("id")
-        if symbol and coin_id:
-            
-            if symbol not in symbol_map:
-                symbol_map[symbol] = coin_id
-    return symbol_map
+        if name and coin_id:
+            name_map[name] = coin_id
+
+    return  name_map
 
 
-symbol_map = build_symbol_map()
+
+
+
+
+
+
+
+
+
+
+name_map = build_symbol_map()
 
 
 def crypto_tool(coin:str):
@@ -41,22 +68,27 @@ def crypto_tool(coin:str):
             return {"Error": "Coin Name is required"}
         
         coin = coin.strip().lower()
-        if coin in symbol_map:
-            coin=symbol_map[coin]
-        url=f"https://api.coingecko.com/api/v3/simple/price?ids={coin}&vs_currencies=usd&include_market_cap=true&include_24hr_change=true"
+        if coin in trusted_symbols:
+            coin_id = trusted_symbols [coin]
+
+        # 2️⃣ Full coin name (bitcoin, ethereum, etc.)
+        elif coin in name_map:
+            coin_id = name_map[coin]
+       
+        url=f"https://api.coingecko.com/api/v3/simple/price?ids={coin_id}&vs_currencies=usd&include_market_cap=true&include_24hr_change=true"
         response = requests.get(url, timeout=10)
         if response.status_code != 200:
             return {"Error": f"Failed to fetch data from API: {response.status_code}"}
         
         data = response.json()
-        if coin not in data:
+        if coin_id not in data:
             return {"status": "ERROR",
                    
             "message":"Coin not found or is not supported",
-            }
-        info=data[coin]
+            } 
+        info=data[coin_id]
         return {
-            "crypto": coin,
+            "crypto": coin_id,
             "price_usd": info.get("usd"),
             "market_cap": info.get("usd_market_cap"),
             "change_24h": info.get("usd_24h_change")
